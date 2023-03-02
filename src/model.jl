356e353d833115c4f12a14d8060c4374d427d94d
SanteriVtj <santeri.vaataja@gmai.com>
2023-03-02 12:04:51 +0200

    nu also in constructor

--- src/model.jl
+++ src/model.jl
@@ -5,11 +5,11 @@ struct RenewalModel{T<:Number}
     draw::Vector{Float64}
     ν::Float64
 
-    function RenewalModel{T}(expiration, fees, max_time, draw) where T<:Number
+    function RenewalModel{T}(expiration, fees, max_time, draw, ν) where T<:Number
         if max_time != first(size(fees))
             error("Fees need to be defined for each period")
         else
-            new(expiration, fees, max_time, draw)
+            new(expiration, fees, max_time, draw, ν)
         end
     end
 end
