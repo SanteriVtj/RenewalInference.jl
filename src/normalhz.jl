f7927b8c073a66e9700b004a290f98eae2169642
SanteriVtj <santeri.vaataja@gmai.com>
2023-05-18 13:55:04 +0300

    Gives domain error for x1

--- src/normalhz.jl
+++ src/normalhz.jl
@@ -1,18 +1,45 @@
-using Distributions, StatsBase
+using Distributions, StatsBase, QuasiMonteCarlo, Random
 
-function simulate_normhz(par, d=Normal(), seed=123)
+function simulate_normhz(par, d=Nothing, seed=123, 
+    z=100, lb=0, ub=1, sample_method=SobolSample())
     Random.seed!(seed)
-    n=par.n;c=par.c
+    μ=par.μ;σ=par.σ;n=par.n;c=par.c
+    if d == Nothing
+        d = Normal(μ, σ)
+    end
 
     x = repeat(rand(d, n), 1, length(c))
     x = sum(x.-c' .≥ 0, dims=2)
 
     x1 = countmap(x)
-    x1 = []
+    x1 = [get(x1, i, Nothing) for i=0:length(c)]
 
-    return x
+    if Nothing ∈ x1
+        throw(DomainError(Nothing))
+    end
+
+    y = sum(x1).-cumsum(x1)
+    y = [sum(x1); y[1:length(y)-1]]
+    y = x1 ./ y
+
+    return (
+            y,
+            QuasiMonteCarlo.sample(z,lb,ub,sample_method)
+        )
 end
 
-function normhz()
-    return
+function normhz(x, par)
+    μ=x[1];σ=x[2];
+    ν=par.ν;n=par.n;c=par.c;y=par.y;z=par.z;
+
+    z = μ+z*σ
+    r_d = (z.-c).≥0
+    
+    ℓ = cumprod(1 ./(1+exp(-(z.-c)/ν)), dims=2)
+
+    survive = sum(ℓ)
+
+    ℓ = n.-survive
+    ℓ = ℓ.-[0;ℓ[1:length(ℓ)-1]]
+
 end
