d924f502ebb256ccb524912735a886daee426e7d
SanteriVtj <santeri.vaataja@gmai.com>
2023-05-23 20:32:17 +0300

    functions from runmodel exported if local system

--- src/RenewalInference.jl
+++ src/RenewalInference.jl
@@ -5,9 +5,16 @@ module RenewalInference
 
     include("model.jl")
     include("normalhz.jl")
+    if pwd()=="C:\\Users\\Santeri\\.julia\\dev\\RenewalInference" include("runmodel.jl") end
 
-    export NormModel,
+    if pwd()=="C:\\Users\\Santeri\\.julia\\dev\\RenewalInference"
+        export NormModel,
         simulate_normhz,
-        normhz
-
+        normhz,
+        runnorm
+    else    
+        export NormModel,
+            simulate_normhz,
+            normhz
+    end
 end
