8547890118e45ee76c36bd72e2cf163c6af1ba42
SanteriVtj <santeri.vaataja@gmai.com>
2023-06-08 17:56:43 +0300

    patenthz

--- src/patenthz.jl
+++ src/patenthz.jl
@@ -14,15 +14,20 @@ function patenthz(par::PatentModel, hz, s, o)
     S = length(s)
     T = length(hz)
 
-    r = zeros(eltype(s), S, T)
     r_d = falses(S, T) # Equivalent of zeros(UInt8,n,m), but instead of UInt8 stores elements as single bits
 
-     # Conversion of mean and variance for log normal distribution according to the normal specification
-     e_mean = ϕ.^(1:T)*σⁱ*(1-γ)
-     e_var = e_mean.^2
+    # Conversion of mean and variance for log normal distribution according to the normal specification
+    e_mean = ϕ.^(1:T)*σⁱ*(1-γ)
+    e_var = e_mean.^2
  
-     μ = 2*log.(e_mean)-1/2*log.(e_mean.^2+e_var)
-     σ = sqrt.(-2*log.(e_mean)+log.(e_var+e_mean.^2))
+    μ = 2*log.(e_mean)-1/2*log.(e_mean.^2+e_var)
+    σ = sqrt.(-2*log.(e_mean)+log.(e_var+e_mean.^2))
+
+    r = s.*σ.+μ
+    r_d[:,1] = r[:,1] .≥ th[1]
+    obsolence = o .≥ θ
+
+    r, r_d = compute_vf()
 end
 
 function simulate_patenthz(par::PatentModel, x, s)
@@ -65,16 +70,28 @@ function simulate_patenthz(par::PatentModel, x, s)
     # Computing patent values for t=2,…,T
     @inbounds for t=2:T
         # compute patent value at t by maximizing between learning shocks and depreciation
-        r[:,t] .= s[:,t-1].*maximum(hcat(δ.*r[:,t-1], learning[:,t-1]), dims=2) # concat as n×2 matrix and choose maximum in for each row
+        r[:,t] .= obsolence[:,t-1].*maximum(hcat(δ.*r[:,t-1], learning[:,t-1]), dims=2) # concat as n×2 matrix and choose maximum in for each row
         # If patent wasn't active in t-1 it cannot be active in t
         r[r_d[:,t-1] .== 0,t] .= 0
         # Patent is kept active if its value exceed the threshold o.w. set to zero
         r_d[:,t] .= r[:,t] .≥ th[t]
     end
 
-    (r, r_d)
+    (computehz(r_d), r, r_d)
 end
 
-function thresholds(par)
-    collect(Float64, 35:5:80)
+function compute_vf(par::PatentModel, r, r_d, learning, obsolence, th, T, likelihood=true)
+    for t=2:T
+        # compute patent value at t by maximizing between learning shocks and depreciation
+        r[:,t] .= s[:,t-1].*maximum(hcat(δ.*r[:,t-1], learning[:,t-1]), dims=2) # concat as n×2 matrix and choose maximum in for each row
+        # If patent wasn't active in t-1 it cannot be active in t
+        r[r_d[:,t-1] .== 0,t] .= 0
+        # Patent is kept active if its value exceed the threshold o.w. set to zero
+        r_d[:,t] .= r[:,t] .≥ th[t]
+    end
+    if likelihood
+        return (r, r_d, ℓ)
+    else
+        return  (r, r_d)
+    end
 end
