a0ea85559d82cb0e1f66af77850298ffd39c8a8d
SanteriVtj <santeri.vaataja@gmai.com>
2023-06-08 20:24:04 +0300

    patenthz works at least superficially

--- src/patenthz.jl
+++ src/patenthz.jl
@@ -7,14 +7,15 @@ function patenthz(par::PatentModel, hz, s, o)
     s: Simulation draw.
     o: Obsolence draw.
     """
-    ϕ=par.ϕ;σⁱ=par.σⁱ;γ=par.γ;δ=par.δ;θ=par.θ;
+    ϕ=par.ϕ;σⁱ=par.σⁱ;γ=par.γ;δ=par.δ;θ=par.θ;ν=par.ν;
 
-    th = thresholds(par)
+    r̄ = thresholds(par)
 
     S = length(s)
-    T = length(hz)
+    T = length(hz)-1
+    r = zeros(S,T)
 
-    r_d = falses(S, T) # Equivalent of zeros(UInt8,n,m), but instead of UInt8 stores elements as single bits
+    r_d = falses(S,T) # Equivalent of zeros(UInt8,n,m), but instead of UInt8 stores elements as single bits
 
     # Conversion of mean and variance for log normal distribution according to the normal specification
     e_mean = ϕ.^(1:T)*σⁱ*(1-γ)
@@ -23,11 +24,25 @@ function patenthz(par::PatentModel, hz, s, o)
     μ = 2*log.(e_mean)-1/2*log.(e_mean.^2+e_var)
     σ = sqrt.(-2*log.(e_mean)+log.(e_var+e_mean.^2))
 
-    r = s.*σ.+μ
-    r_d[:,1] = r[:,1] .≥ th[1]
+    s = exp.(s.*σ'.+μ')
+    r[:,1] = s[:,1]
+    r_d[:,1] = r[:,1] .≥ r̄[1]
     obsolence = o .≥ θ
 
-    r, r_d = compute_vf()
+    ℓ = zeros(size(r))
+    ℓ[:,1] = 1 ./(1 .+exp.(r[:,1]./ν))
+
+    @inbounds for t=2:T
+        # compute patent value at t by maximizing between learning shocks and depreciation
+        r[:,t] .= obsolence[:,t-1].*maximum(hcat(δ.*r[:,t-1], s[:,t-1]), dims=2) # concat as n×2 matrix and choose maximum in for each row
+        # If patent wasn't active in t-1 it cannot be active in t
+        r[r_d[:,t-1] .== 0,t] .= 0
+        # Patent is kept active if its value exceed the threshold o.w. set to zero
+        r_d[:,t] .= r[:,t] .≥ r̄[t]
+        ℓ[:,t] = likelihood(r, r̄, t, ν)
+    end
+
+    modelhz(sum(ℓ, dims=1)', S)
 end
 
 function simulate_patenthz(par::PatentModel, x, s)
@@ -38,15 +53,19 @@ function simulate_patenthz(par::PatentModel, x, s)
 
         # Arguments:
         par::PatentModel: struct containing parameters for the distribution of patent exirations.
-        x: random matrix distributed as U([0,1]) for drawing the values from LogNormal.
-        s: obsolence draw. Also U([0,1]) distributed random matrix.
+        x: random matrix distributed as U([0,1]) for drawing the values from LogNormal. Size N×T
+        s: obsolence draw. Also U([0,1]) distributed random matrix. Size N×T-1
     """
     ϕ=par.ϕ;σⁱ=par.σⁱ;γ=par.γ;δ=par.δ;θ=par.θ;
-    th = threshold(par)
+    th = thresholds(par)
 
     n, T = size(x)
+    m, k = size(s)
     q = @view x[:,1]
     z = @view x[:,2:end]
+
+    @assert length(th) == T == k+1 "Dimension mismatch in time"
+    @assert n == m "Dimension mismatch in the size of sample between x and s"
 
     # Conversion of mean and variance for log normal distribution according to the normal specification
     e_mean = ϕ.^(1:T)*σⁱ*(1-γ)
@@ -77,21 +96,10 @@ function simulate_patenthz(par::PatentModel, x, s)
         r_d[:,t] .= r[:,t] .≥ th[t]
     end
 
-    (computehz(r_d), r, r_d)
+    (computehz(sum(r_d, dims=2)), r, r_d)
 end
 
-function compute_vf(par::PatentModel, r, r_d, learning, obsolence, th, T, likelihood=true)
-    for t=2:T
-        # compute patent value at t by maximizing between learning shocks and depreciation
-        r[:,t] .= s[:,t-1].*maximum(hcat(δ.*r[:,t-1], learning[:,t-1]), dims=2) # concat as n×2 matrix and choose maximum in for each row
-        # If patent wasn't active in t-1 it cannot be active in t
-        r[r_d[:,t-1] .== 0,t] .= 0
-        # Patent is kept active if its value exceed the threshold o.w. set to zero
-        r_d[:,t] .= r[:,t] .≥ th[t]
-    end
-    if likelihood
-        return (r, r_d, ℓ)
-    else
-        return  (r, r_d)
-    end
-end
+
+likelihood(r, r̄, t, ν) = prod(1 ./(1 .+exp.(-(r[:,1:t-1].-r̄[1:t-1]')./ν)),dims=2).*1 ./(1 .+exp.((r[:,t].-r̄[t])./ν))
+
+
