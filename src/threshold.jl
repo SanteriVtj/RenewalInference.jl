fe955fbbfa02b481778113f2372504b7f3a75dea
SanteriVtj <santeri.vaataja@gmai.com>
2023-06-15 21:23:25 +0300

    interpolation

--- src/threshold.jl
+++ src/threshold.jl
@@ -21,11 +21,15 @@ function thresholds(par, c, z, o, increment=5)
     μ, σ = log_norm_parametrisation(par, T)
 
     z = exp.(z.*σ'.+μ')
+    o = o .≤ θ
     
     # @inbounds
     for t=T-1:-1:1
         temp1 = repeat(δ.*r1', N)
         temp2 = repeat(z[:,t],1,ngrid)
+        temp3 = repeat(s[:,t],1,ngrid)
+        interp = linear_interpolation(r1, V(:,t))
+        temp4 = interp(temp3.*maximum([temp2;;;temp3], dims=3))
     end
     [116, 138, 169, 201, 244, 286, 328, 381, 445, 508, 572, 646, 720, 794, 868, 932, 995]
 end
