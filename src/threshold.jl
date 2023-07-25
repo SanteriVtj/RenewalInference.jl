83b36084fddba99c46012b47a0315ca8bbc516ce
SanteriVtj <santeri.vaataja@gmai.com>
2023-07-25 19:16:52 +0300

    idx bug fixed

--- src/threshold.jl
+++ src/threshold.jl
@@ -32,7 +32,7 @@ function thresholds(par, c, z, o, β, increment=5)
         temp5 = 1/N.*ones(1,N)*temp4
         V[t,:] = r1'.-c[t].+β.*temp5
         idx = findfirst(V[t,:].>zero(eltype(V)))
-        r̄[t] = idx == 1 ? 0. : (r1[idx-1]*V[t,idx]-r1[idx]*V[t,idx-1])/(V[t,idx]-V[t,idx-1])
+        r̄[t] = (idx == 1) | isnothing(idx) ? 0. : (r1[idx-1]*V[t,idx]-r1[idx]*V[t,idx-1])/(V[t,idx]-V[t,idx-1])
         V[t,:] = maximum([V[t,:] zeros(size(V[t,:]))], dims=2)
     end
     r̄
