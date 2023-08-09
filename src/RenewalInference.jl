b8f6a33592debc8d318a2572cac681db776e5ece
SanteriVtj <santeri.vaataja@gmai.com>
2023-08-09 20:31:21 +0300

    local exports excluded

--- src/RenewalInference.jl
+++ src/RenewalInference.jl
@@ -10,28 +10,11 @@ module RenewalInference
     include("patenthz.jl")
     include("hz_functions.jl")
     
-    if pwd()=="C:\\Users\\Santeri\\.julia\\dev\\RenewalInference" 
-        
-        include("runmodel.jl") 
-        
-        export NormModel,
+    
+    export NormModel,
         simulate_normhz,
         normhz,
-        runnorm,
         PatentModel,
-        simulate_patenthz,
-        patenthz,
-        runpatent,
-        runpm,
-        computehz,
-        thresholds,
-        log_norm_parametrisation
-    else    
-        export NormModel,
-            simulate_normhz,
-            normhz,
-            PatentModel,
-            simulate_pathenthz,
-            patenthz
-    end
+        simulate_pathenthz,
+        patenthz
 end
