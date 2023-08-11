8ccc5ac77c12058f27af8580c1db6729cbe84d9e
SanteriVtj <santeri.vaataja@gmai.com>
2023-08-11 17:18:19 +0300

    simulation wrapper

--- src/util.jl
+++ src/util.jl
@@ -1,6 +1,6 @@
 function sample_patenthz(x0, hz, c; β=.95, ν=2)
-    obsolence = QuasiMonteCarlo.sample(N,T-1,QuasiMonteCarlo.HaltonSample())';
-    ishock = QuasiMonteCarlo.sample(N,1,QuasiMonteCarlo.HaltonSample())';
+    obsolence = QuasiMonteCarlo.sample(N,T-1,QuasiMonteCarlo.HaltonSample())'
+    ishock = QuasiMonteCarlo.sample(N,1,QuasiMonteCarlo.HaltonSample())'
     return patenthz(
         x0,
         hz,
@@ -9,5 +9,18 @@ function sample_patenthz(x0, hz, c; β=.95, ν=2)
         c,
         β=β,
         ν=ν
+    )
+end
+
+function create_simul_hz(par)
+    simulation_shocks = QuasiMonteCarlo.sample(N,T,QuasiMonteCarlo.HaltonSample())'
+    obsolence = QuasiMonteCarlo.sample(N,T-1,QuasiMonteCarlo.HaltonSample())'
+    ishock = QuasiMonteCarlo.sample(N,1,QuasiMonteCarlo.HaltonSample())'
+    return simulate_patenthz(
+        par,
+        simulation_shocks,
+        obsolence,
+        c,
+        ishock
     )
 end
