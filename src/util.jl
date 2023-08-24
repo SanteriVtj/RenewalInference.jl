f7e78095c55d2939681a88aa5da82cc6214dc741
SanteriVtj <santeri.vaataja@gmai.com>
2023-08-24 20:50:53 +0300

    antithetic sample corrected to have correct dimensions

--- src/util.jl
+++ src/util.jl
@@ -1,6 +1,7 @@
 function _patenthz(x0, hz, c; β=.95, ν=2, N=200, T=17, alg=QuasiMonteCarlo.HaltonSample())
     obsolence = QuasiMonteCarlo.sample(N,T-1,alg)'
     ishock = quantile.(Normal(), QuasiMonteCarlo.sample(N÷2,1,alg)')
+    ishock = [ishock;-ishock]
     return patenthz(
         x0,
         hz,
