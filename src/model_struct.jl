f0ef331c972b52e8ffa3821985aa7a7577d6ea82
SanteriVtj <santeri.vaataja@gmai.com>
2023-11-24 20:35:31 +0100

    Valuefunction matrix

--- src/model_struct.jl
+++ src/model_struct.jl
@@ -9,8 +9,10 @@ struct ModelData{T<:AbstractFloat}
     ν::T
     β::T
     nt::Int
+    ngrid::Int
+    V::Matrix{T}
     function ModelData{T}(
-        hz, costs, X, x, obsolence, r, r_d; 
+        hz, costs, X, x, obsolence, r, r_d, ngrid, V;
         ν=2., β=.95, nt=Threads.nthreads()) where {T<:AbstractFloat}
         # Define dimensions
         t = length(hz)
@@ -22,7 +24,8 @@ struct ModelData{T<:AbstractFloat}
         t == size(obsolence, 2) + 1 ? nothing : throw(AssertionError("Length of hazards and obsolences doesn't match."))
         t == size(r, 2) ? nothing : throw(AssertionError("Length of hazards and simulation time doesn't match."))
         t == size(r_d, 2) ? nothing : throw(AssertionError("Length of hazards and simulation dummy time doesn't match."))
-
+        t == size(V, 1) ? nothing : throw(AssertionError("Length of hazards and value discretised value functions time dimension doesn't match."))
+        
         # Test that each variable has correct sample size
         N == size(obsolence, 1) ? nothing : throw(AssertionError("Number of observations and simulation obsolences doesn't match."))
         N == size(x, 1) ? nothing : throw(AssertionError("Number of observations and simulation value sample doesn't match."))
@@ -30,12 +33,12 @@ struct ModelData{T<:AbstractFloat}
         N == size(r_d, 1) ? nothing : throw(AssertionError("Number of observations and simulation dummies doesn't match."))
         
         # If all tests are satisfied, create new instance
-        new(hz, costs, X, x, obsolence, r, r_d, ν, β, nt)
+        new(hz, costs, X, x, obsolence, r, r_d, ν, β, nt, ngrid, V)
     end
 end
 
 function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
-    alg=QuasiMonteCarlo.HaltonSample()) where {T<:AbstractFloat}
+    alg=QuasiMonteCarlo.HaltonSample(), ngrid=500) where {T<:AbstractFloat}
     # Inference dimensions for simulation draws
     N = size(X, 1)
     t = length(hz)
@@ -47,7 +50,10 @@ function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
     r = zeros(T,N,t)
     r_d = falses(N, t)
 
+    # Discretisation for value function
+    V = Matrix(undef, t, ngrid)
+
     return ModelData{T}(
-        hz, costs, X, x, obsolence, r, r_d
+        hz, costs, X, x, obsolence, r, r_d, ngrid, V
     )
 end
