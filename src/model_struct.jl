402bf93e80ad0a8af1a495f020407b50fff7bab1
SanteriVtj <santeri.vaataja@gmai.com>
2023-11-26 13:36:57 +0100

    nt removed

--- src/model_struct.jl
+++ src/model_struct.jl
@@ -11,17 +11,12 @@ struct ModelData{T<:AbstractFloat}
     X::Matrix{T}
     x::Matrix{T}
     obsolence::Matrix{T}
-    r::Matrix{T}
-    r_d::BitMatrix
     ν::T
     β::T
-    nt::Int
     ngrid::Int
-    V::Matrix{T}
     controller::ModelControl
     function ModelData{T}(
-        hz, costs, X, x, obsolence, r, r_d, ngrid, V,
-        nt, controller;
+        hz, costs, X, x, obsolence, ngrid, controller;
         ν=2., β=.95
     ) where {T<:AbstractFloat}
         # Define dimensions
@@ -32,26 +27,20 @@ struct ModelData{T<:AbstractFloat}
         t == size(x, 2) ? nothing : throw(AssertionError("Length of hazards and simulation periods doesn't match."))
         t == length(costs) ? nothing : throw(AssertionError("Length of hazards and costs doesn't match."))
         t == size(obsolence, 2) + 1 ? nothing : throw(AssertionError("Length of hazards and obsolences doesn't match."))
-        t == size(r, 2) ? nothing : throw(AssertionError("Length of hazards and simulation time doesn't match."))
-        t == size(r_d, 2) ? nothing : throw(AssertionError("Length of hazards and simulation dummy time doesn't match."))
-        t == size(V, 1) ? nothing : throw(AssertionError("Length of hazards and value discretised value functions time dimension doesn't match."))
         
         # Test that each variable has correct sample size
         N == size(obsolence, 1) ? nothing : throw(AssertionError("Number of observations and simulation obsolences doesn't match."))
         N == size(x, 1) ? nothing : throw(AssertionError("Number of observations and simulation value sample doesn't match."))
-        N == size(r, 1) ? nothing : throw(AssertionError("Number of observations and preallocation for values doesn't match."))
-        N == size(r_d, 1) ? nothing : throw(AssertionError("Number of observations and simulation dummies doesn't match."))
         
         # If all tests are satisfied, create new instance
-        new(hz, costs, X, x, obsolence, r, r_d, ν, β, nt, ngrid, V, controller)
+        new(hz, costs, X, x, obsolence, ν, β, ngrid, controller)
     end
 end
 
 # Function for generating new instance of ModelData with default (Halton)samples
 # and other preallocated matrices.
 function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
-    alg=QuasiMonteCarlo.HaltonSample(), ngrid=500,
-    nt=Threads.nthreads(), controller=ModelControl()
+    alg=QuasiMonteCarlo.HaltonSample(), ngrid=500, controller=ModelControl()
 ) where {T<:AbstractFloat}
     # Inference dimensions for simulation draws
     N = size(X, 1)
@@ -60,14 +49,8 @@ function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
     # Generate quasi monte carlo draws
     obsolence = Matrix(QuasiMonteCarlo.sample(N,t-1,alg)')
     x = Matrix(QuasiMonteCarlo.sample(N,t,alg)')
-    # Preallocate memory for value function matrices
-    r = zeros(T,N,t)
-    r_d = falses(N, t)
-
-    # Discretisation for value function
-    V = Matrix(undef, t, ngrid)
 
     return ModelData{T}(
-        hz, costs, X, x, obsolence, r, r_d, ngrid, V, nt, controller
+        hz, costs, X, x, obsolence, ngrid, controller
     )
 end
