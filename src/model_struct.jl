e553bcbc62157837c86576c1746fb8c231d32581
SanteriVtj <santeri.vaataja@gmai.com>
2023-12-08 13:52:41 +0100

    data for innovations sigma

--- src/model_struct.jl
+++ src/model_struct.jl
@@ -9,15 +9,17 @@ struct ModelData{T<:AbstractFloat}
     hz::Vector{T}
     costs::Vector{T}
     X::Matrix{T}
+    s_data::Matrix{T}
     x::Matrix{T}
     obsolence::Matrix{T}
     ν::T
     β::T
     ngrid::Int
     controller::ModelControl
+    alg::Union{Sampleable, SamplingAlgorithm}
     function ModelData{T}(
-        hz, costs, X, x, obsolence, ngrid, controller;
-        ν=2., β=.95
+        hz, costs, X, s_data, x, obsolence, ngrid, controller;
+        ν=2., β=.95, alg=QuasiMonteCarlo.HaltonSample()
     ) where {T<:AbstractFloat}
         # Define dimensions
         t = length(hz)
@@ -31,15 +33,16 @@ struct ModelData{T<:AbstractFloat}
         # Test that each variable has correct sample size
         N == size(obsolence, 1) ? nothing : throw(AssertionError("Number of observations and simulation obsolences doesn't match."))
         N == size(x, 1) ? nothing : throw(AssertionError("Number of observations and simulation value sample doesn't match."))
+        N == size(s_data, 1) ? nothing : throw(AssertionError("Number of observations and data for σⁱ doesn't match."))
         
         # If all tests are satisfied, create new instance
-        new(hz, costs, X, x, obsolence, ν, β, ngrid, controller)
+        new(hz, costs, X, s_data, x, obsolence, ν, β, ngrid, controller, alg)
     end
 end
 
 # Function for generating new instance of ModelData with default (Halton)samples
 # and other preallocated matrices.
-function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
+function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T}, s_data::Matrix{T};
     alg=QuasiMonteCarlo.HaltonSample(), ngrid=500, controller=ModelControl()
 ) where {T<:AbstractFloat}
     # Inference dimensions for simulation draws
@@ -51,6 +54,11 @@ function ModelData(hz::Vector{T}, costs::Vector{T}, X::Matrix{T};
     x = Matrix(QuasiMonteCarlo.sample(N,t,alg)')
 
     return ModelData{T}(
-        hz, costs, X, x, obsolence, ngrid, controller
+        hz, costs, X, s_data, x, obsolence, ngrid, controller, alg=alg
     )
+end
+
+function repopulate_x!(md::ModelData)
+    N,K = size(md.x)
+    @views md.x[:,:] = Matrix(QuasiMonteCarlo.sample(N,K,md.alg)')
 end
