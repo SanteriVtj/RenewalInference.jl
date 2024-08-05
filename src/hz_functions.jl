79bea7ef5df6965e11e37c7e3006a7115a52fcef
SanteriVtj <santeri.vaataja@gmai.com>
2024-08-05 21:25:59 +0300

    doesn't return nans

--- src/hz_functions.jl
+++ src/hz_functions.jl
@@ -13,5 +13,7 @@ end
 function modelhz(x, N)
     cum_lapse = N .- x
     lapse = cum_lapse .- [zero(eltype(x)); cum_lapse[1:end-1]]
-    return lapse ./ [N; x[1:end-1]]
+    hz = lapse ./ [N; x[1:end-1]]
+    hz[isnan.(hz)] .= 0
+    return hz
 end
